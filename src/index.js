c5c29079615f2d0439baf43c558452b847a571f3
Masha <valzyr48@gmail.com>
2019-10-07 14:20:26 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  
+  let answer=[];
+  let numbers_f=[];
+  let numbers_s=[];
+  numbers_f=first.split('');
+  numbers_s=second.split('');
+  numbers_f.reverse();
+  numbers_s.reverse();
+  let multiple=[];
+  for (i=0; i<numbers_f.length; i++) {
+    for (j=0; j<numbers_s.length; j++) {
+      multiple[i+j]= multiple[i+j]? multiple[i+j]+numbers_f[i]*numbers_s[j] : numbers_f[i]*numbers_s[j];
+    }
+  }
+
+  for (i=0; i<multiple.length; i++) {
+    if (multiple[i]>=10)
+    {
+      answer[i]=multiple[i]%10;
+      multiple[i+1]= multiple[i+1] ? multiple[i+1] + Number.parseInt(multiple[i]/10) :Number.parseInt(multiple[i]/10);
+
+    } 
+    else {
+        if (!answer[i]) {
+          answer[i]=0;
+          answer[i]=answer[i] + multiple[i];
+        }
+        else {
+          answer[i]=answer[i] + multiple[i];
+        }
+     } 
+
+  }
+  let string=answer.reverse().join('');
+  return string;
+
 }
